db7f37ab9ddbc0ec302bb51d590f14e8fe54ddf0
kelvin711 <arauzkelvin99@gmail.com>
2022-08-01 11:46:24 -0500

    express lecture

---
+++ Week_04/Day_01/First_express/server.js
@@ -0,0 +1,50 @@
+const express = require("express"); //getting our express function
+const cors = require("cors") //bringing our cors dependency
+const app = express() //setting app to the invocation of express function
+
+app.use(express.json()) // allows us to use JSON
+app.use(cors()) // enables us to make calls to ourselfs
+
+// app.get
+// app.post
+// app.put / patch 
+// app.delete
+// app.get(route, a_function_to_do_things)
+const magic_set = [
+    { id: 1, set_name: "KTK"},
+    { id: 2, set_name: "2X2"},
+    { id: 3, set_name: "RAV"},
+]
+
+
+app.get("/api/magic_set", (request, respond) => {
+    respond.send(magic_set)
+})
+
+app.post("/api/magic_set", (request, response) => {
+    // console.log(request.body);
+    // const set_to_add = {
+    //     id: magic_set.length + 1,
+    //     set_name: request
+    // }
+    magic_set.push(request.body)
+    response.status(200).json({message: "ok"})
+})
+// { id: 1, set_name: "KTK"},
+app.put("/api/magic_set/:set_id", (request, response)=> {
+    console.log(request);
+    const set = magic_set.find( (set_object) => set_object.id === parseInt(request.params.set_id))
+    set.set_name = request.body.set_name
+    response.status(200).json({message: "ok"})
+})
+
+app.delete("/api/magic_set/:set_id", (request, response) => {
+    const set = magic_set.find( (set_object) => set_object.id === parseInt(request.params.set_id))
+    magic_set.splice(request.params.set_id -1, 1)
+    response.send("you did it")
+})
+
+app.listen( 8000, () => {
+    console.log("we are live....");
+})// puts our server live
+
--- Week_04/Day_01/decode_string.js
+++ Week_04/Day_01/decode_string.js
@@ -16,3 +16,31 @@
 // >>> "3ce" // "fh"
 // >>> "2fcjjm" // "hello"
 
+const caesar_cypher = (input_string) => {
+    //                0    5  7
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let idx = 0;
+    let new_char = "";
+    let encoded_string = "";
+    let num = parseInt(input_string[0]);
+    for (let i = 1; i < input_string.length ;i++) {
+        let character = input_string[i]; //
+        // encoded_string = alphabet[character.indexOf + num]
+        for (let j = 1; j < alphabet.length; j++) {
+            let alphabet_letter = alphabet[i]
+            if ( alphabet_letter === character ) {
+                idx = j;
+                console.log(idx);
+                break;
+            }     
+        // encoded_string = alphabet[idx + num]
+        }
+        // let rotate_index = idx + num;
+        // console.log(rotate_index);
+        // new_char = alphabet[rotate_index]
+    }
+    return encoded_string;
+}
+console.log(caesar_cypher("2fcjjm"));
+
+
