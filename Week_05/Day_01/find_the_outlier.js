454271e6edfbd8e75c067dc2778a7ab8b83f8037
kelvin711 <arauzkelvin99@gmail.com>
2022-08-08 09:34:57 -0500

    find the outlier js

--- Week_05/Day_01/find_the_outlier.js
+++ Week_05/Day_01/find_the_outlier.js
@@ -14,3 +14,55 @@
 
 // [160, 3, 1719, 19, 11, 13, -21] Should return: 160 (the only even number)
 
+let array = [2, 4, 8, 8, 3, 6, 6]
+
+let challengefunction = (array) => {
+
+    let checkereven = 0
+    let checkerodd = 0
+
+    //only check 3 elements
+    for (let i = 0; i < 3; i++) {
+        if (array[i] % 2 == 0) {
+            checkereven += 1
+        }
+        if (array[i] % 2 !== 0) {
+            checkerodd += 1
+        }
+        if (checkereven >= 2) {
+            let found = array.find(element => element % 2 != 0)
+            return (found)
+        }
+        if (checkerodd >= 2) {
+            let found = array.find(element => element % 2 == 0)
+            return (found)
+        }
+    }
+}
+console.log(challengefunction([2, 4, 0, 100, 4, 11, 2602, 36]))
+
+
+function findingOutlier(arr) {
+    let evenArr = [];
+    let oddArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            evenArr += arr[i]
+        }
+        else {
+            oddArr += arr[i]
+        }
+    }
+    if (evenArr.length > oddArr.length) {
+        return oddArr
+    } else {
+        return evenArr
+    }
+}
+
+const findTheOddBall = (array) => {
+    const isEven = array[0] % 2  === 0;
+    while (isEven && (array[index] % 2 === 0 || !isEven && array[index++] % 2 != 0))
+    return array[index];
+}
